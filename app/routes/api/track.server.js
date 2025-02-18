b3cfdf7eb9dac293343f3ba3a8af9fa43ba809e6
Noah Müller <ChefNoah@MacBook-Air.local>
2025-02-18 16:21:29 +0100

    tracking

---
+++ app/routes/api/track.server.js
@@ -0,0 +1,38 @@
+// app/routes/api/track.server.js
+import { json } from "@remix-run/node";
+import mongoose from "mongoose";
+
+// Stelle sicher, dass du die Verbindung nur einmal herstellst.
+// Falls die Verbindung bereits besteht, verwende sie.
+if (!mongoose.connection.readyState) {
+  mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+// Definiere ein Schema für Tracking-Daten
+const trackingSchema = new mongoose.Schema({
+  transaction_id: String,
+  total: Number,
+  currency: String,
+  createdAt: { type: Date, default: Date.now },
+});
+
+// Um Fehler zu vermeiden, wenn das Model mehrfach definiert wird, prüfe, ob es bereits existiert
+const Tracking =
+  mongoose.models.Tracking || mongoose.model("Tracking", trackingSchema);
+
+// Exportiere die action-Funktion, die ausschließlich auf dem Server läuft
+export async function action({ request }) {
+  try {
+    const data = await request.json();
+    console.log("Tracking-Daten empfangen:", data);
+    const trackingEvent = new Tracking(data);
+    await trackingEvent.save();
+    return json({ message: "Tracking-Daten empfangen" });
+  } catch (error) {
+    console.error("Error saving tracking data:", error);
+    return json({ message: "Internal Server Error" }, { status: 500 });
+  }
+}
